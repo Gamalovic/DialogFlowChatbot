db5769f42de83d8f98ef5f26c530448d79cb2adf
Gamalovic <omar.gamalfciscu@gmail.com>
2017-11-29 19:32:02 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,15 +2,9 @@ export const environment = {
   production: true,
 
   dialogFlow:{
-    chatBot:'697ecb15a0814881ab3c9ce6b3127ce0'
+    //your dialogflow configuration
   },
-
   firebase:{
-    apiKey: "AIzaSyC6oKgcrXDjqe54czMeoN2zqzYbxG8y0Ys",
-    authDomain: "floki-8d54b.firebaseapp.com",
-    databaseURL: "https://floki-8d54b.firebaseio.com",
-    projectId: "floki-8d54b",
-    storageBucket: "floki-8d54b.appspot.com",
-    messagingSenderId: "735717106865"
+    //your firebase configuration
   }
 };
